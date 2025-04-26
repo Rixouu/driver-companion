07bf59da3bd8422f6716ceeeb5244982cc3bec0b
Rixouu <admin.rixou@gmail.com>
2025-04-26 14:54:52 +0700

    Fix container padding and margin

--- components/layout/page-container.tsx
+++ components/layout/page-container.tsx
@@ -1,6 +1,6 @@
 export function PageContainer({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex-1 container mx-auto p-4 md:p-6 lg:pl-72 space-y-8">
+    <main className="flex-1 container mx-auto px-8 sm:px-10 md:px-12 py-4 md:py-6 lg:pl-72 space-y-8 max-w-[1600px]">
       {children}
     </main>
   )

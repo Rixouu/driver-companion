7d858b64d49c2905016ab943b08936d749c1f609
Rixouu <41510824+Rixouu@users.noreply.github.com>
2025-04-28 15:00:22 +0700

    Delete sql directory

--- sql/timestamps.sql
+++
@@ -1,8 +0,0 @@
--- Function to automatically set updated_at timestamp for tables
-CREATE OR REPLACE FUNCTION trigger_set_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql; 
